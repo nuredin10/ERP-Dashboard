3daa1a960305137eca78c9f9a70a5b128aecf742
Naatty123 <nattynengeda@gmail.com>
2022-11-18 15:45:44 +0300

    Add form Completed

--- src/pages/procurment/addpurchasedmaterial.js
+++ src/pages/procurment/addpurchasedmaterial.js
@@ -205,16 +205,20 @@ const Addpurchasedmaterial = () => {
                   <Grid
                     container
                     spacing={2}
+                    // columns={{xs: 4, md: 3}}
                     sx={{
                       boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
                       ml: 3,
                       mt: 3,
                       backgroundColor: "white",
                       pb: 2,
+                      pr: 4,
                       borderRadius: "10px",
+                      // display: 'grid'
+
                     }}
                   >
-                    <Grid item sm={6} md={2} lg={4}>
+                    <Grid item sm={6} md={2} lg={3}>
                       <TextField
                         required
                         name="new_name"
@@ -225,7 +229,7 @@ const Addpurchasedmaterial = () => {
                         fullWidth
                       />
                     </Grid>
-                    <Grid item sm={6} md={2} lg={4}>
+                    <Grid item sm={6} md={2} lg={3}>
                       <TextField
                         fullWidth
                         required
@@ -236,7 +240,7 @@ const Addpurchasedmaterial = () => {
                         onChange={(event) => handleFormChange(index, event)}
                       />
                     </Grid>
-                    <Grid item sm={6} md={2} lg={4}>
+                    <Grid item sm={6} md={2} lg={3}>
                       <TextField
                         fullWidth
                         required
@@ -247,8 +251,9 @@ const Addpurchasedmaterial = () => {
                         onChange={(event) => handleFormChange(index, event)}
                       />
                     </Grid>
-                    <Grid item sm={6} md={2} lg={2}>
+                    <Grid item sm={6} md={2} lg={3}>
                       <TextField
+                        fullWidth
                         required
                         name="new_materialcode"
                         label="MaterialCode"
@@ -257,8 +262,9 @@ const Addpurchasedmaterial = () => {
                         onChange={(event) => handleFormChange(index, event)}
                       />
                     </Grid>
-                    <Grid item sm={6} md={2} lg={2}>
+                    <Grid item sm={6} md={2} lg={3}>
                       <TextField
+                        fullWidth
                         required
                         name="new_spec"
                         label="Specification"
@@ -280,6 +286,7 @@ const Addpurchasedmaterial = () => {
                     </Grid>
                     <Grid item sm={6} md={2} lg={3}>
                       <TextField
+                        fullWidth
                         required
                         name="new_value"
                         label="Value"
@@ -288,8 +295,9 @@ const Addpurchasedmaterial = () => {
                         onChange={(event) => handleFormChange(index, event)}
                       />
                     </Grid>
-                    <Grid item sm={6} md={2} lg={2}>
+                    <Grid item sm={6} md={2} lg={3}>
                       <TextField
+                        fullWidth
                         required
                         name="new_referncenum"
                         label="Reference Number"
@@ -298,7 +306,7 @@ const Addpurchasedmaterial = () => {
                         onChange={(event) => handleFormChange(index, event)}
                       />
                     </Grid>
-                    <Grid item sm={6} md={2} lg={2}>
+                    <Grid item sm={6} md={2} lg={3}>
                       {/* <TextField
                         required
                         name="new_materialtype"
@@ -320,8 +328,9 @@ const Addpurchasedmaterial = () => {
                         <MenuItem value="ACCS">ACCS</MenuItem>
                       </Select>
                     </Grid>
-                    <Grid item sm={6} md={2} lg={2}>
+                    <Grid item sm={6} md={2} lg={3}>
                       <TextField
+                        fullWidth
                         required
                         name="payable_name"
                         label="Payable Name"
@@ -330,7 +339,7 @@ const Addpurchasedmaterial = () => {
                         onChange={(event) => handleFormChange(index, event)}
                       />
                     </Grid>
-                    <Grid item sm={6} md={2} lg={2}>
+                    <Grid item sm={6} md={2} lg={3}>
                       <TextField
                         required
                         fullWidth
@@ -342,8 +351,9 @@ const Addpurchasedmaterial = () => {
                       />
                     </Grid>
 
-                    <Grid item sm={6} md={2} lg={2}>
+                    <Grid item sm={6} md={2} lg={3}>
                       <TextField
+                        fullWidth
                         required
                         name="new_remark"
                         label="Remark"
@@ -353,7 +363,7 @@ const Addpurchasedmaterial = () => {
                       />
                     </Grid>
 
-                    <Grid item xs={1} lg={1} sm={1} md={1} sx={{ mt: "-2%" }}>
+                    <Grid item xs={1} lg={2} sm={1} md={1} sx={{ mt: "2%", ml: "2%" }}>
                       <IconButton onClick={() => removeFields(index)}>
                         <RemoveIcon />
                       </IconButton>
