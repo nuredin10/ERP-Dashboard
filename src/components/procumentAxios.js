045dcbb4100d0d61de7f5b2723baab56c22533a7
Aklilegetachew <aklilegetachew@gmail.com>
2023-02-21 11:49:40 +0300

    again deployment

--- src/components/procumentAxios.js
+++ src/components/procumentAxios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:11000"
+    // baseURL: "http://localhost:11000"
 
-    // baseURL: "https://report.proplast.et",
+    baseURL: "https://report.proplast.et",
 })
 
 export default instance;
