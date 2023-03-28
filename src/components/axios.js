b31c7ea01a4fd061a3235348bc1b504df69923fc
Aklilegetachew <aklilegetachew@gmail.com>
2023-03-28 16:55:09 +0300

    production url

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:11000"
-    // baseURL: "https://report.proplast.et",
+    // baseURL: "http://localhost:11000"
+    baseURL: "https://report.proplast.et",
 })
 
 export default instance;
